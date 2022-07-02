a15b0b76174a302e752289c2fea8a773e268f3fa
Aidan Prior <sifiwizard.code@gmail.com>
2022-07-02 13:54:32 -0600

    User seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) --NEEDS passowrd to change to hash but for now it is fine
+VALUES ('Alice', 'alice@mail.com', 'password')
+('Bob', 'bod@mail.com', 'password')
+('Joy', 'joy@mail.com', 'password')
+('Steve', 'steve@mail.com', 'password')
+('Yu', 'yu@mail.com', 'password')
+('Damian', 'damian@mail.com', 'password')
