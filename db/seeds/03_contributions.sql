7eb366d76d3240d40f069dd2ee9c5beabac78485
Aidan Prior <sifiwizard.code@gmail.com>
2022-07-02 14:47:08 -0600

    Stories and contribution seeds

---
+++ db/seeds/03_contributions.sql
@@ -0,0 +1,5 @@
+INSERT INTO contributions (story_id, additions)
+VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim Polemonis. At hoc in eo M. Nec vero alia sunt quaerenda contra Carneadeam illam sententiam. Memini me adesse P. Si qua in iis corrigere voluit, deteriora fecit. Duo Reges: constructio interrete. Summae mihi videtur inscitiae. Huius ego nunc auctoritatem sequens idem faciam. Quibus ego vehementer assentior.')
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sint ista Graecorum; Erat enim Polemonis. Pugnant Stoici cum Peripateticis. Laboro autem non sine causa;')
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat enim Polemonis. Quonam modo? Vitae autem degendae ratio maxime quidem illis placuit quieta. Duo Reges: constructio interrete.')
