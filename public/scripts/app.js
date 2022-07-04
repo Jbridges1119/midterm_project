b6d21f64a53a55687308e4f2c0f0deb4ed957864
Jeff Bridges <jbridges1119@gmail.com>
2022-07-04 04:23:53 +0000

    Refactor scrips

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,38 @@
 // Client facing scripts here
+$(document).ready(function() {
+  //MOVES WINDOW TO TOP OF PAGE
+  $(`.scrollTop`).click(toTop);
+
+  //FADES IN/OUT SCROLLTOP BUTTON
+  $(window).scroll(toTopFade);
+
+  $(window).scroll(storyStaysAtTop);
+  $("i.report").click(report);
+
+})
+
+
+const storyStaysAtTop = function(){
+  const $story = $('#myStory');
+  const $title = $('p.title')
+  $story.toggleClass('stationary', $(this).scrollTop() > $title.height())
+};
+
+//CHANGES REPORT FLAG TO RED WHEN CLICKED
+const report = function(){
+  $(this).css('color', `red`)
+};
+
+//MOVES WINDOW TO TOP OF PAGE
+const toTop = function(event) {
+  event.preventDefault();
+  $("html").animate({ scrollTop: 0}, 'fast');
+};
+
+//FADES IN/OUT SCROLLTOP BUTTON
+const toTopFade = function() {
+  if ($(this).scrollTop() > 80) {
+    return $('.hidden').fadeIn();
+  }
+  return $('.hidden').fadeOut();
+};
