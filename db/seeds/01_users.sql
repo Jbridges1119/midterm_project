17686e136f9271712c70c0cdce0be31543590784
Aidan Prior <sifiwizard.code@gmail.com>
2022-07-04 12:32:43 -0600

    ENV changed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 -- Users table seeds here (Example)
 INSERT INTO users (id, name, email, password) --NEEDS passowrd to change to hash but for now it is fine
-VALUES (1, 'Alice', 'alice@mail.com', 'password')
-(2, 'Bob', 'bod@mail.com', 'password')
-(3, 'Joy', 'joy@mail.com', 'password')
-(4, 'Steve', 'steve@mail.com', 'password')
-(5, 'Yu', 'yu@mail.com', 'password')
-(6, 'Damian', 'damian@mail.com', 'password')
+VALUES (1, 'Alice', 'alice@mail.com', 'password'),
+(2, 'Bob', 'bod@mail.com', 'password'),
+(3, 'Joy', 'joy@mail.com', 'password'),
+(4, 'Steve', 'steve@mail.com', 'password'),
+(5, 'Yu', 'yu@mail.com', 'password'),
+(6, 'Damian', 'damian@mail.com', 'password');
