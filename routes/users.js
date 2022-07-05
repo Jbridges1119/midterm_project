d1920a214f465230af84ee82b0ed5fa67971aff4
Aidan Prior <sifiwizard.code@gmail.com>
2022-07-05 16:38:39 -0600

    User routes

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,34 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/:id", (req, res) => {
+    db.query(`
+    SELECT *
+    FROM stories
+    where user_id = ${id}
+    `)
+    .then(data => {
+      const userStories = data.rows;
+      res.json({ stories });
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({erroe: err.message})''
+    });
+  });
+
+  router.post("/:id", (req,res)=>{
+    db.query(`
+    INSERT INTO contributions (story_id, additions)
+    VALUES (${id}, ${req.body.additions});
+    `)
+    .catch(err => {
+      res
+      .status(500)
+      .json({erroe: err.message})''
+    });
+  })
   return router;
 };
